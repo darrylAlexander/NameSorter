ebb8e3d344a2825f45c411f59e218a43bf97959c
Darryl Alexander <mr.darryl.alexander@gmail.com>
2019-05-12 22:44:17 +0100

    Reads from user input to get path and prints
    Reads from the user input and opens a streamreader. This then writes
    each line into a list. The list is then looped through and each entry
    printed.
    
    	modified:   NameSorter/NameSorter.csproj
    	modified:   NameSorter/Program.cs
    	new file:   NameSorter/src/ReadFromFile/ReadFromFile.cs

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NameSorter.src.ReadFromFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace NameSorter
 {
@@ -6,7 +9,15 @@ namespace NameSorter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter the location of the file to read from.");
+            var pathToFile = Console.ReadLine();
+            var fileReader = new ReadFromFile(pathToFile);
+            List<string> linesFromFile = fileReader.OpenAndReadFileContents();
+            foreach (var line in linesFromFile)
+            {
+                Console.WriteLine(line);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ NameSorter/src/ReadFromFile/ReadFromFile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NameSorter.src.ReadFromFile
+{
+    public class ReadFromFile
+    {
+        public ReadFromFile(string pathToFile)
+        {
+            _pathToFile = pathToFile;
+        }
+
+        public List<string> OpenAndReadFileContents()
+        {
+            var listOfLineContents = new List<string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(_pathToFile))
+                {
+                    String line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        listOfLineContents.Add(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                listOfLineContents.Add("The file could not be read: " + e.Message);
+            }
+
+            return listOfLineContents;
+        }
+
+        // Properties
+        public string PathToFile { get; set; }
+
+        // Fields
+        private readonly string _pathToFile;
+    }
+}
