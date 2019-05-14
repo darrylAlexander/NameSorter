b14c8bf21152fbb9763e5501c483ff3a7b5c11d5
Darryl Alexander <mr.darryl.alexander@gmail.com>
2019-05-14 22:49:16 +0100

    Edit: Made sure it writes contents to text file
    Program now writes to sorted-names-list.txt in the current working
    directory.
    
    	modified:   Program.cs

--- NameSorter/Program.cs
+++ NameSorter/Program.cs
@@ -3,6 +3,7 @@ using NameSorter.src.OrderNames;
 using NameSorter.src.ReadFromFile;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace NameSorter
 {
@@ -12,16 +13,23 @@ namespace NameSorter
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Please pass the path of the file to read from.");
+                Console.WriteLine("Please add the path of the file to read from as an argument.");
                 return;
             }
 
             List<Name> namesFromFile = ReadFromFile.OpenAndReadFileContents(args[0]);
-            List<Name> namesSortedByLastName = new OrderNames().OrderByLastName(namesFromFile);
-            foreach (var name in namesSortedByLastName)
+            List<Name> namesSorted = new OrderNames().OrderByLastName(namesFromFile);
+
+            var filePath = Directory.GetCurrentDirectory() + "\\sorted-names-list.txt";
+            var fileContents = "";
+
+            foreach (var name in namesSorted)
             {
+                fileContents += name.ToString() + "\r\n";
                 Console.WriteLine(name.ToString());
             }
+
+            File.WriteAllText(filePath, fileContents);
         }
     }
 }
