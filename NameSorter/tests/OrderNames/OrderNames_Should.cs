a6da271bd5a2ebe4ca0d899a1015ec9f59aea244
Darryl Alexander <mr.darryl.alexander@gmail.com>
2019-05-14 17:13:18 +0100

    Add: added unit test for OrderNames
    Added unit test that implements passing scenario
    OrderNames.OrderByLastName
    
    	new file:   NameSorter/tests/OrderNames/OrderNames_Should.cs

---
+++ NameSorter/tests/OrderNames/OrderNames_Should.cs
@@ -0,0 +1,31 @@
+﻿using NameSorter.src.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NameSorter.tests.OrderNames
+{
+    public class OrderNames_Should
+    {
+        [Fact]
+        public void ReturnAListOrderedByLastName()
+        {
+            // Arrange
+            var nameSorter = new src.OrderNames.OrderNames();
+            var lastNameBeginningWithB = new Name("Bob", "Bananas");
+            var lastNameBeginningWithA = new Name("Alice", "Apple");
+
+            var names = new List<Name>();
+            names.Add(lastNameBeginningWithB);
+            names.Add(lastNameBeginningWithA);
+
+            // Act
+            var systemUnderTest = nameSorter.OrderByLastName(names);
+            var actual = systemUnderTest.IndexOf(lastNameBeginningWithA);
+
+            // Assert
+            Assert.Equal(0, actual);
+        }
+    }
+}
